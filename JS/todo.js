8551d1bbf1b9852686c53b238b458f833f3ad9bc
Yeonsu Lee <linege1234@gmail.com>
2023-03-12 23:27:08 +1100

    todos done

--- JS/todo.js
+++ JS/todo.js
@@ -14,8 +14,12 @@ function todoSbumit(event) {
   event.preventDefault();
   const newTodo = toInput.value;
   toInput.value = "";
-  toDos.push(newTodo);
-  paindToDo(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paindToDo(newTodoObj);
   saveToDos();
 }
 
@@ -24,12 +28,15 @@ function deleteToDo(event) {
   //parentelement는 어떤 리스트인지 확인해주는거야 진짜 중요 !!!!!
   const li = event.target.parentElement;
   li.remove();
+  toDos = toDos.filter((todo) => todo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 function paindToDo(todo) {
   const li = document.createElement("li");
+  li.id = todo.id;
   const span = document.createElement("span");
-  span.innerText = todo;
+  span.innerText = todo.text;
   const button = document.createElement("button");
   button.innerText = "X";
   button.addEventListener("click", deleteToDo); //이건 이벤트가 나오기를 기다리고 있는 함수
@@ -46,5 +53,5 @@ if (savedToDos !== null) {
   //json.parse 모두 오브젝트화 시켜줌/오브젝트는 foreach 가능
   const parsedToDos = JSON.parse(savedToDos);
   toDos = parsedToDos;
-  parsedToDos.forEach(paindToDo);
+  parsedToDos.forEach(paindToDo); //foreach 는 array에서 모든 요소들을 파라미터로 어떤함수를 계속실행하는거야
 }
